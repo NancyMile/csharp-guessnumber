4cd29b051da7816c5006da6c759c150d256ad642
nancy <smithnancy@outlook.com>
2023-10-01 13:22:48 +1100

    guess number  validate input

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+// See https://aka.ms/new-console-template for more information
+
+StartGame();
+
+void StartGame()
+{
+   Console.WriteLine("Guess the number!");
+   Console.WriteLine("What is your name?");
+   var playerName = Console.ReadLine();
+   WantToPlay(playerName);
+}
+
+void WantToPlay(string? playerName)
+{
+     Console.WriteLine($"Hi {playerName} are you ready? (Enter Yes/No)");
+    var wantToPlay = Console.ReadLine();
+
+    switch (wantToPlay?.ToLower())
+    {
+        case "yes":
+            Play();
+            break;
+        case "no":
+            DontPlay();
+            break;
+        default:
+            Console.WriteLine("Not a valid option, Please enter yes or not");
+            WantToPlay(playerName);
+            break;
+    }
+}
+
+void Play()
+{
+    Console.WriteLine("Play");
+}
+
+void DontPlay()
+{
+    Console.WriteLine("Not Play");
+}
