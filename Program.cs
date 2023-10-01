2cecd67aefe5d9178edcd193050add9054f24519
nancy <smithnancy@outlook.com>
2023-10-01 14:03:30 +1100

    Control extructure and generate a random number

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 // See https://aka.ms/new-console-template for more information
 
 StartGame();
@@ -33,10 +34,40 @@ void WantToPlay(string? playerName)
 
 void Play()
 {
-    Console.WriteLine("Play");
+    Random random = new Random();
+    int guessNumber = random.Next(1,10);
+
+    try
+    {
+        Console.WriteLine("Please enter a number between 1 and 10");
+        var playerNumber = Console.ReadLine();
+
+        if(playerNumber is null)
+            throw new Exception("Please enter a number");
+        if(int.Parse(playerNumber) < 1 || int.Parse(playerNumber) > 10)
+            throw new Exception("Enter a number between 1 and 10");
+    }
+    catch (Exception e)
+    {
+         Console.WriteLine($"Error: {e.Message}");
+         Play();
+    }
 }
 
 void DontPlay()
 {
     Console.WriteLine("Stop Playing ..");
+}
+
+void GuessNumber(int guessNumber)
+{
+    Console.WriteLine("Please enter a number between 1 and 10");
+    var playerNumber = int.Parse(Console.ReadLine());
+
+    if(playerNumber == guessNumber)
+        Console.WriteLine($"You Won the number is {guessNumber}");
+    else if(playerNumber < guessNumber)
+        Console.WriteLine("The number is higher");
+    else
+        Console.WriteLine("The number is lower");
 }
