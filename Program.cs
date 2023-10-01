32bc6d8bd4efbe0e53612518c9b1aa7e6d3f91fb
nancy <smithnancy@outlook.com>
2023-10-01 14:18:28 +1100

    structure numbers validation

--- Program.cs
+++ Program.cs
@@ -9,10 +9,14 @@ void StartGame()
    Console.WriteLine("Guess the number!");
    Console.WriteLine("What is your name?");
    var playerName = Console.ReadLine();
-   WantToPlay(playerName);
+
+   Random random = new Random();
+   int guessNumber = random.Next(1,10);
+
+   WantToPlay(playerName, guessNumber);
 }
 
-void WantToPlay(string? playerName)
+void WantToPlay(string? playerName, int guessNumber)
 {
      Console.WriteLine($"Hi {playerName} are you ready? (Enter Yes/No)");
     var wantToPlay = Console.ReadLine();
@@ -20,23 +24,20 @@ void WantToPlay(string? playerName)
     switch (wantToPlay?.ToLower().Trim())
     {
         case "yes":
-            Play();
+            Play(guessNumber);
             break;
         case "no":
             DontPlay();
             break;
         default:
             Console.WriteLine("Not a valid option, Please enter yes or not");
-            WantToPlay(playerName);
+            WantToPlay(playerName,guessNumber);
             break;
     }
 }
 
-void Play()
+void Play(int guessNumber)
 {
-    Random random = new Random();
-    int guessNumber = random.Next(1,10);
-
     try
     {
         Console.WriteLine("Please enter a number between 1 and 10");
@@ -46,28 +47,28 @@ void Play()
             throw new Exception("Please enter a number");
         if(int.Parse(playerNumber) < 1 || int.Parse(playerNumber) > 10)
             throw new Exception("Enter a number between 1 and 10");
+
+        if(int.Parse(playerNumber) == guessNumber)
+            Console.WriteLine($"You Won the number is {guessNumber}");
+        else if(int.Parse(playerNumber) < guessNumber)
+        {
+            Console.WriteLine("The number is higher");
+            Play(guessNumber);
+        }
+        else
+        {
+            Console.WriteLine("The number is lower");
+            Play(guessNumber);
+        }
     }
     catch (Exception e)
     {
          Console.WriteLine($"Error: {e.Message}");
-         Play();
+         Play(guessNumber);
     }
 }
 
 void DontPlay()
 {
     Console.WriteLine("Stop Playing ..");
-}
-
-void GuessNumber(int guessNumber)
-{
-    Console.WriteLine("Please enter a number between 1 and 10");
-    var playerNumber = int.Parse(Console.ReadLine());
-
-    if(playerNumber == guessNumber)
-        Console.WriteLine($"You Won the number is {guessNumber}");
-    else if(playerNumber < guessNumber)
-        Console.WriteLine("The number is higher");
-    else
-        Console.WriteLine("The number is lower");
 }
